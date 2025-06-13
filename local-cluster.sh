eb6d504f08a3e7c9aa11ef6f30a29f35d5da54be
zg1r <zg1r@prorobotech.ru>
2025-06-13 19:38:09 +0500

    web: fix istio

--- local-cluster.sh
+++ local-cluster.sh
@@ -2,13 +2,13 @@
 
 LOCAL_CLUSTER_NAME="incloud-k8s-local-dev-local-1"
 
-echo "--- start minikube with CNI=cilium"
-minikube start \
-  --cni=cilium \
-  --dns-domain=${LOCAL_CLUSTER_NAME}.incloud.x5.ru
-  # --extra-config=apiserver.oidc-issuer-url="https://dex.incloud-idp.svc.${LOCAL_CLUSTER_NAME}.incloud.x5.ru" \
-  # --extra-config=apiserver.oidc-username-claim=email \
-  # --extra-config=apiserver.oidc-client-id=incloud \
+# echo "--- start minikube with CNI=cilium"
+# minikube start \
+#   --cni=cilium \
+#   --dns-domain=${LOCAL_CLUSTER_NAME}.incloud.x5.ru
+#   # --extra-config=apiserver.oidc-issuer-url="https://dex.incloud-idp.svc.${LOCAL_CLUSTER_NAME}.incloud.x5.ru" \
+#   # --extra-config=apiserver.oidc-username-claim=email \
+#   # --extra-config=apiserver.oidc-client-id=incloud \
 
 TMP_DIR=$(mktemp -d -p /tmp)
 echo "DEBUG: tmp directory: ${TMP_DIR} "
@@ -30,200 +30,200 @@ export \
   CLUSTER_AREA=local \
   CLUSTER_INDEX=1
 
-echo ""
-echo "--- create admin role & admin user"
-kubectl apply -f - <<EOF
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  name: admin-clusterrole
-rules:
-  - apiGroups:
-      - "*"
-    resources:
-      - "*"
-    verbs:
-      - "*"
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: admin-clusterrolebinding
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: admin-clusterrole
-subjects:
-  - apiGroup: rbac.authorization.k8s.io
-    kind: User
-    name: admin@x5.ru
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: anonymous-clusterrolebinding
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: admin-clusterrole
-subjects:
-- apiGroup: rbac.authorization.k8s.io
-  kind: User
-  name: system:anonymous
-EOF
+# echo ""
+# echo "--- create admin role & admin user"
+# kubectl apply -f - <<EOF
+# ---
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRole
+# metadata:
+#   name: admin-clusterrole
+# rules:
+#   - apiGroups:
+#       - "*"
+#     resources:
+#       - "*"
+#     verbs:
+#       - "*"
+# ---
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRoleBinding
+# metadata:
+#   name: admin-clusterrolebinding
+# roleRef:
+#   apiGroup: rbac.authorization.k8s.io
+#   kind: ClusterRole
+#   name: admin-clusterrole
+# subjects:
+#   - apiGroup: rbac.authorization.k8s.io
+#     kind: User
+#     name: admin@x5.ru
+# ---
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRoleBinding
+# metadata:
+#   name: anonymous-clusterrolebinding
+# roleRef:
+#   apiGroup: rbac.authorization.k8s.io
+#   kind: ClusterRole
+#   name: admin-clusterrole
+# subjects:
+# - apiGroup: rbac.authorization.k8s.io
+#   kind: User
+#   name: system:anonymous
+# EOF
 
 
-echo ""
-echo "--- templating cilium"
-helm template cilium ./charts/cilium/cilium-${CILIUM_VERSION}/cilium \
-  --namespace kube-system \
-  --set operator.replicas=1 \
-  --set ipam.operator.clusterPoolIPv4PodCIDRList="10.244.0.0/16" \
-  --set ipam.operator.clusterPoolIPv4MaskSize=24 > ${TMP_DIR}/cilium.yaml
-echo "--- deploy cilium"
-kubectl apply -f ${TMP_DIR}/cilium.yaml
+# echo ""
+# echo "--- templating cilium"
+# helm template cilium ./charts/cilium/cilium-${CILIUM_VERSION}/cilium \
+#   --namespace kube-system \
+#   --set operator.replicas=1 \
+#   --set ipam.operator.clusterPoolIPv4PodCIDRList="10.244.0.0/16" \
+#   --set ipam.operator.clusterPoolIPv4MaskSize=24 > ${TMP_DIR}/cilium.yaml
+# echo "--- deploy cilium"
+# kubectl apply -f ${TMP_DIR}/cilium.yaml
 
 
-echo ""
-echo "--- templating cert-manager"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=cert-manager \
-  template > ${TMP_DIR}/cert-manager.yaml incloud-collections: "cert-manager"
-echo "--- deploy cert-manager"
-kubectl create ns incloud-cert-manager
-kubectl apply -f ${TMP_DIR}/cert-manager.yaml
-echo "----- waiting finish cert-manager startupapicheck"
-kubectl -n incloud-cert-manager wait job/cert-manager-startupapicheck --for=jsonpath='{.status.succeeded}'=1 --timeout=180s
-kubectl apply -f ${TMP_DIR}/cert-manager.yaml
-echo "----- waiting run cert-manager webhook"
-kubectl -n incloud-cert-manager wait deployment/cert-manager-webhook --for=jsonpath='{.status.readyReplicas}'=1 --timeout=180s
-kubectl apply -f ${TMP_DIR}/cert-manager.yaml # деплоит сертификаты, которые в прошлую итерацию не создались из-за не работающей валидации
+# echo ""
+# echo "--- templating cert-manager"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=cert-manager \
+#   template > ${TMP_DIR}/cert-manager.yaml incloud-collections: "cert-manager"
+# echo "--- deploy cert-manager"
+# kubectl create ns incloud-cert-manager
+# kubectl apply -f ${TMP_DIR}/cert-manager.yaml
+# echo "----- waiting finish cert-manager startupapicheck"
+# kubectl -n incloud-cert-manager wait job/cert-manager-startupapicheck --for=jsonpath='{.status.succeeded}'=1 --timeout=180s
+# kubectl apply -f ${TMP_DIR}/cert-manager.yaml
+# echo "----- waiting run cert-manager webhook"
+# kubectl -n incloud-cert-manager wait deployment/cert-manager-webhook --for=jsonpath='{.status.readyReplicas}'=1 --timeout=180s
+# kubectl apply -f ${TMP_DIR}/cert-manager.yaml # деплоит сертификаты, которые в прошлую итерацию не создались из-за не работающей валидации
 
 
-echo ""
-echo "--- templating istio"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=istio \
-  template > ${TMP_DIR}/istio.yaml
-echo "--- deploy istio"
-kubectl create ns incloud-istio
-kubectl -n incloud-istio apply -f ${TMP_DIR}/istio.yaml
-echo "--- waiting run istiod"
-kubectl -n incloud-istio wait deployment/istiod --for=jsonpath='{.status.readyReplicas}'=1 --timeout=180s
-for i in {1..2}
-do
-  kubectl -n incloud-istio apply -f ${TMP_DIR}/istio.yaml
-done
+# echo ""
+# echo "--- templating istio"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=istio \
+#   template > ${TMP_DIR}/istio.yaml
+# echo "--- deploy istio"
+# kubectl create ns incloud-istio
+# kubectl -n incloud-istio apply -f ${TMP_DIR}/istio.yaml
+# echo "--- waiting run istiod"
+# kubectl -n incloud-istio wait deployment/istiod --for=jsonpath='{.status.readyReplicas}'=1 --timeout=180s
+# for i in {1..2}
+# do
+#   kubectl -n incloud-istio apply -f ${TMP_DIR}/istio.yaml
+# done
 
 
-echo ""
-echo "--- templating dex"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=dex \
-  template > ${TMP_DIR}/dex.yaml
-echo "--- deploy dex"
-kubectl create ns incloud-idp
-for i in {1..3}
-do
-  kubectl -n incloud-idp apply -f ${TMP_DIR}/dex.yaml
-done
+# echo ""
+# echo "--- templating dex"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=dex \
+#   template > ${TMP_DIR}/dex.yaml
+# echo "--- deploy dex"
+# kubectl create ns incloud-idp
+# for i in {1..3}
+# do
+#   kubectl -n incloud-idp apply -f ${TMP_DIR}/dex.yaml
+# done
 
 
-echo ""
-echo "--- templating netbox"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=netbox \
-  template > ${TMP_DIR}/netbox.yaml
-echo "--- deploy netbox"
-kubectl create ns incloud-netbox
-for i in {1..3}
-do
-  kubectl -n incloud-netbox apply -f ${TMP_DIR}/netbox.yaml
-done
+# echo ""
+# echo "--- templating netbox"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=netbox \
+#   template > ${TMP_DIR}/netbox.yaml
+# echo "--- deploy netbox"
+# kubectl create ns incloud-netbox
+# for i in {1..3}
+# do
+#   kubectl -n incloud-netbox apply -f ${TMP_DIR}/netbox.yaml
+# done
 
 
-echo ""
-echo "--- templating sgroups"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=sgroups \
-  template > ${TMP_DIR}/sgroups.yaml
-echo "--- deploy sgroups"
-kubectl create ns incloud-sgroups
-for i in {1..3}
-do
-  kubectl -n incloud-sgroups apply -f ${TMP_DIR}/sgroups.yaml
-done
+# echo ""
+# echo "--- templating sgroups"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=sgroups \
+#   template > ${TMP_DIR}/sgroups.yaml
+# echo "--- deploy sgroups"
+# kubectl create ns incloud-sgroups
+# for i in {1..3}
+# do
+#   kubectl -n incloud-sgroups apply -f ${TMP_DIR}/sgroups.yaml
+# done
 
 
-echo ""
-echo "--- templating sgroups-provider"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=sgroups-provider \
-  template > ${TMP_DIR}/sgroups-provider.yaml
-echo "--- deploy sgroups-provider"
-kubectl create ns incloud-sgroups
-for i in {1..3}
-do
-  kubectl -n incloud-sgroups apply -f ${TMP_DIR}/sgroups-provider.yaml
-done
+# echo ""
+# echo "--- templating sgroups-provider"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=sgroups-provider \
+#   template > ${TMP_DIR}/sgroups-provider.yaml
+# echo "--- deploy sgroups-provider"
+# kubectl create ns incloud-sgroups
+# for i in {1..3}
+# do
+#   kubectl -n incloud-sgroups apply -f ${TMP_DIR}/sgroups-provider.yaml
+# done
 
 
-echo ""
-echo "--- templating sgroups-resources"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=sgroups-resources \
-  template > ${TMP_DIR}/sgroups-resources.yaml
-echo "--- deploy sgroups-resources"
-kubectl create ns incloud-sgroups
-for i in {1..3}
-do
-  kubectl -n incloud-sgroups apply -f ${TMP_DIR}/sgroups-resources.yaml
-done
+# echo ""
+# echo "--- templating sgroups-resources"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=sgroups-resources \
+#   template > ${TMP_DIR}/sgroups-resources.yaml
+# echo "--- deploy sgroups-resources"
+# kubectl create ns incloud-sgroups
+# for i in {1..3}
+# do
+#   kubectl -n incloud-sgroups apply -f ${TMP_DIR}/sgroups-resources.yaml
+# done
 
 
-echo ""
-echo "--- templating sgroups-to-nft"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=to-nft \
-  template > ${TMP_DIR}/sgroups-to-nft.yaml
-echo "--- deploy sgroups-to-nft"
-kubectl create ns incloud-sgroups
-for i in {1..3}
-do
-  kubectl -n incloud-sgroups apply -f ${TMP_DIR}/sgroups-to-nft.yaml
-done
+# echo ""
+# echo "--- templating sgroups-to-nft"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=to-nft \
+#   template > ${TMP_DIR}/sgroups-to-nft.yaml
+# echo "--- deploy sgroups-to-nft"
+# kubectl create ns incloud-sgroups
+# for i in {1..3}
+# do
+#   kubectl -n incloud-sgroups apply -f ${TMP_DIR}/sgroups-to-nft.yaml
+# done
 
 
-echo ""
-echo "--- templating netguard"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=netguard \
-  template > ${TMP_DIR}/netguard.yaml
-echo "--- deploy netguard"
-kubectl create ns incloud-sgroups
-for i in {1..3}
-do
-  kubectl -n incloud-sgroups apply -f ${TMP_DIR}/netguard.yaml
-done
+# echo ""
+# echo "--- templating netguard"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=netguard \
+#   template > ${TMP_DIR}/netguard.yaml
+# echo "--- deploy netguard"
+# kubectl create ns incloud-sgroups
+# for i in {1..3}
+# do
+#   kubectl -n incloud-sgroups apply -f ${TMP_DIR}/netguard.yaml
+# done
 
 
 echo ""
@@ -241,38 +241,38 @@ do
 done
 
 
-echo ""
-echo "--- templating crossplane"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=crossplane \
-  template > ${TMP_DIR}/crossplane.yaml
-echo "--- deploy crossplane"
-kubectl create ns incloud-crossplane
-kubectl -n incloud-crossplane apply -f ${TMP_DIR}/crossplane.yaml
-kubectl -n incloud-crossplane wait deployment/crossplane-crossplane --for=jsonpath='{.status.readyReplicas}'=1 --timeout=180s
-for i in {1..2}
-do
-  kubectl -n incloud-crossplane apply -f ${TMP_DIR}/crossplane.yaml --server-side
-  sleep 10
-done
+# echo ""
+# echo "--- templating crossplane"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=crossplane \
+#   template > ${TMP_DIR}/crossplane.yaml
+# echo "--- deploy crossplane"
+# kubectl create ns incloud-crossplane
+# kubectl -n incloud-crossplane apply -f ${TMP_DIR}/crossplane.yaml
+# kubectl -n incloud-crossplane wait deployment/crossplane-crossplane --for=jsonpath='{.status.readyReplicas}'=1 --timeout=180s
+# for i in {1..2}
+# do
+#   kubectl -n incloud-crossplane apply -f ${TMP_DIR}/crossplane.yaml --server-side
+#   sleep 10
+# done
 
 
-echo ""
-echo "--- templating crossplane-incloud"
-helmfile \
-  -e dev \
-  --kube-version=${K8S_VERSION} \
-  -l incloud-collections=crossplane-incloud \
-  template > ${TMP_DIR}/crossplane-incloud.yaml
-echo "--- deploy crossplane-incloud"
-kubectl create ns incloud-crossplane
-for i in {1..10}
-do
-  kubectl -n incloud-crossplane apply -f ${TMP_DIR}/crossplane-incloud.yaml && break
-  sleep 30
-done
+# echo ""
+# echo "--- templating crossplane-incloud"
+# helmfile \
+#   -e dev \
+#   --kube-version=${K8S_VERSION} \
+#   -l incloud-collections=crossplane-incloud \
+#   template > ${TMP_DIR}/crossplane-incloud.yaml
+# echo "--- deploy crossplane-incloud"
+# kubectl create ns incloud-crossplane
+# for i in {1..10}
+# do
+#   kubectl -n incloud-crossplane apply -f ${TMP_DIR}/crossplane-incloud.yaml && break
+#   sleep 30
+# done
 
 echo ""
 echo ""
