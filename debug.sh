9abfd7b7b73b7cec010d8d57668b45403923f6a7
zg1r <zg1r@prorobotech.ru>
2025-06-20 19:31:34 +0500

    argocd: fix h-bf

--- debug.sh
+++ debug.sh
@@ -17,6 +17,6 @@ source ${TMP_DIR}/components_versions.env
 
 helmfile \
   -e ${CLUSTER_ENV} \
-  -l incloud-collections=argocd \
+  -l incloud-collections=sgroups \
   --kube-version=${K8S_VERSION} \
   template
